c01114d744a27ff52a541e990ea1a912b7524430
Leroy Merlos <leroymerlos@gmail.com>
2019-04-01 19:04:29 +0200

    @miguelorrico @migueldavidorrico Ejercicio 2 parte 1.

---
+++ Packages.java
@@ -0,0 +1,15 @@
+/* ¿Qué es un package en Java?
+ * Un Paquete en Java es un contenedor de clases que
+ * permite agrupar las distintas partes de un programa 
+ * y que por lo general tiene una funcionalidad y elementos comunes
+ * , definiendo la ubicación de dichas clases en 
+ * un directorio de estructura jerárquica.  
+ */
+
+/*¿Qué relación tiene con la sentencia import?
+ * Las clases que están en el mismo paquete no es necesario 
+ * importarlas. A su vez podemos importar un paquete completo.
+ */
+public class Packages {
+
+}
