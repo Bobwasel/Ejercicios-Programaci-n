50402fbcc2e33d95e54cffa4c69144ed3d334470
Leroy Merlos <leroymerlos@gmail.com>
2019-04-06 10:36:13 +0200

    @miguelorrico @migueldavidorrico Ejercicio 4.

---
+++ Finalize.java
@@ -0,0 +1,9 @@
+public class Finalize {
+    /* ¿Para qué sirve el método finalize() de la clase Object?
+    *   Sirve para liberar la memoria del objeto.
+    *  ¿Cuándo es llamado?
+    *   Lo llama el recolector de basura cuando determine que no hay más referencias a ese objeto.
+    *  ¿Para qué se usa?
+    *   Se usa para realizar acciones de limpieza antes de que se destruya el objeto.
+    */
+}
