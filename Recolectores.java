f3265a56325f7b4a99140ea3fa4c983ce6594e30
Leroy Merlos <leroymerlos@gmail.com>
2019-04-06 11:01:13 +0200

    @miguelorrico @migueldavidorrico Ejercicio 7.

---
+++ Recolectores.java
@@ -0,0 +1,12 @@
+public class Recolectores {
+    /*
+    *
+    *  ¿Para qué sirve System.gc()?
+    *   Método que ejecuta el garbage collector del sistema.  
+    *  ¿Cuando se le llama?
+    *   Se llama automáticamente cuando lo determina la JVM o podemos llamarlo nosotros.
+    *  ¿Qué nos asegura?
+    *  El garbage collector realiza un reciclado de 
+    *  los objetos de la memoria de la JVM para optimizar el espacio de la misma.
+    */
+}
