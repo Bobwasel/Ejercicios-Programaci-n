269eabbb42db3e0e7d2ca2d44fbf424730be2e5a
Leroy Merlos <leroymerlos@gmail.com>
2019-04-06 10:44:26 +0200

    @miguelorrico @migueldavidorrico Ejercicio 5.

---
+++ MetodosMath.java
@@ -0,0 +1,8 @@
+public class MetodosMath {
+    /* abs : Toma el valor absoluto de un número.
+    * max : Para hallar el mayor de dos números se emplean las funciones max que compara números del mismo tipo.
+    * min : Para hallar el menor de dos números se emplean las funciones min que compara números del mismo tipo.
+    * pow : Sirve para hacer la potencia de un número. 
+    * round : Retorna el número redondeado al entero más cercano.
+    */
+}
